bf1d14c3020addfbe7755d2acbf2e72dbc29f8b1
Perry Sittser <sittserp@gmail.com>
2020-12-02 21:09:43 -0800

    get-many-characters with .then

--- get-many-characters.js
+++ get-many-characters.js
@@ -2,29 +2,35 @@ const fetch = require('node-fetch')
 const charIds = [1, 4, 5]
 
 
-async function getManyCharacters(someArray) {
-    const res = await fetch(`https://rickandmortyapi.com/api/character/${someArray}`)
-    const json = await res.json()
-    const data = [
-        {
-            name: json[0].name,
-            status: json[0].status,
-            species: json[0].species
-        },
-        {
-            name: json[1].name,
-            status: json[1].status,
-            species: json[1].species
-        },
-        {
-            name: json[2].name,
-            status: json[2].status,
-            species: json[2].species
-        }
+function getManyCharacters(someArray) {
+    return fetch(`https://rickandmortyapi.com/api/character/${someArray}`)
+        .then(res => {
+            return res.json()
+        })
+        .then(json => {
+            return [
+                {
+                    name: json[0].name,
+                    status: json[0].status,
+                    species: json[0].species
+                },
+                {
+                    name: json[1].name,
+                    status: json[1].status,
+                    species: json[1].species
+                },
+                {
+                    name: json[2].name,
+                    status: json[2].status,
+                    species: json[2].species
+                }
 
-    ]
-    console.log(data)
-    return data
+            ]
+        })
+
+
+
+
 }
 
 
