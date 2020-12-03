120de26af7edc5c7cdcc34fd801457ac3c8bc57d
Perry Sittser <sittserp@gmail.com>
2020-12-02 18:11:46 -0800

    get-character test passes!

--- get-character.js
+++ get-character.js
@@ -2,22 +2,20 @@ const fetch = require('node-fetch')
 
 
 function getCharacter(someId) {
-
-    fetch(`https://rickandmortyapi.com/api/character/${someId}`)
+    return fetch(`https://rickandmortyapi.com/api/character/${someId}`)
         .then(res => {
             return res.json()
         })
         .then(json => {
-            const data = {
+            return {
                 name: json.name,
                 status: json.status,
                 species: json.species
             }
-            console.log(data)
-            return data
         })
+
 }
 
 getCharacter(1)
 
-module.exports = getCharacter
+module.exports = getCharacter 
